aac4a424a2e7beae345b262c7ed8e510d1273870
niomoo <jennyliu.lyh@gmail.com>
2022-11-09 17:10:40 +0800

    [DB]Add table patient, patient, record into the database

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -1,92 +1,105 @@
-
-from django.utils import timezone
 import csv
-from interface.models import Patient, Record
+from interface.models import Patient, Dialysis, Record
+from decimal import Decimal
 
 def run():
-    start_time = timezone.now()
-    file_path = 'interface/data/洗腎病人名單.csv'
-    record_path = 'interface/data/202111-202207.csv'
-    # with open(file_path, "r", encoding='utf-8') as csv_file:
-    #     data = csv.reader(csv_file, delimiter=",")
-    #     next(data)                                            # 跳過第一列
-    #     patients = []
-    #     for row in data:
-    #         patient = Patient(
-    #             p_id=row[0],
-    #             p_name=row[1],
-    #         )
-    #         patients.append(patient)
-    #         if len(patients) > 100:
-    #             Patient.objects.bulk_create(patients)         # 減少儲存次數
-    #             patients = []
-    #     if patients:
-    #         Patient.objects.bulk_create(patients)
-    with open(record_path, "r", encoding='utf-8') as csv_file:
+    patient_path = 'interface/data/patient.csv'
+    dialysis_path = 'interface/data/dialysis.csv'
+    record_path = 'interface/data/record.csv'
+    with open(patient_path, "r", encoding='utf-8') as csv_file:
+        data = csv.reader(csv_file, delimiter=",")
+        next(data)                                            # 跳過第一列
+        patients = []
+        for row in data:
+            patient = Patient(
+                p_id = row[1],
+                p_name = row[2],
+                gender = row[3],
+                birth = row[4] 
+            )
+            patients.append(patient)
+            if len(patients) > 100:
+                Patient.objects.bulk_create(patients)         # 減少儲存次數
+                patients = []
+        if patients:
+            Patient.objects.bulk_create(patients)
+    with open(dialysis_path, "r", encoding='utf-8') as csv_file:
         data = csv.reader(csv_file, delimiter=",")
         next(data)
+        dialysis = []
+        for row in data:
+            dialyse = Dialysis(
+                p_id = Patient.objects.get(p_id = row[1]),
+                age = row[2],
+                times = row[3],
+                start_time = row[4],
+                end_time = row[5],
+                machine_id = row[6],
+                bed = row[7],
+                temperature = Decimal(row[8]),
+                start_temperature = Decimal(row[9]),
+                before_weight = Decimal(row[10]),
+                ideal_weight = Decimal(row[11]),
+                expect_dehydration = Decimal(row[12]),
+                transfusion = Decimal(row[13]),
+                food = Decimal(row[14]),
+                estimate_dehydration = Decimal(row[15]),
+                set_dehydration = Decimal(row[16]),
+                after_weight = Decimal(row[17]),
+                real_dehydration = Decimal(row[18]),
+                start_SBP = Decimal(row[19]),
+                start_DBP = row[20],
+                end_SBP = Decimal(row[21]),
+                end_DBP = row[22],
+                mode = row[23],
+                machine = row[24],
+                start_flow_speed = Decimal(row[25]),
+                start_blood_speed = Decimal(row[26]),
+                Ca = Decimal(row[27]),
+                conductivity = Decimal(row[28]),
+                channel = row[29],
+                heparin = row[30],
+                ESA = row[31],
+                coagulation = row[32],
+            )
+            dialysis.append(dialyse)
+        if len(dialysis) > 5000:
+            Dialysis.objects.bulk_create(dialysis)   
+            dialysis = []
+        if dialysis:
+            Dialysis.objects.bulk_create(dialysis)
+    with open(record_path, "r", encoding='utf-8') as csv_file:
+        data = csv.reader(csv_file, delimiter=",")
+        next(data)                                            # 跳過第一列
         records = []
         for row in data:
-            pid = int(row[0])
-            print(pid)
+            d_id = Dialysis.objects.filter(p_id = row[1], times = row[2])
+            if d_id.count() > 1:
+                d_id = d_id[d_id.count() - 1]
+            else:
+                d_id = d_id[0]
             record = Record(
-                p_id = Patient.objects.get(p_id = pid),
-                name = row[1],
-                gender = row[2],
-                birth = row[3],
-                age = row[4],
-                times = row[5],
-                start_time =row[6],
-                end_time = row[7],
-                record_time = row[8],
-                machine_id = row[9],
-                bed = row[10],
-                temperature = row[11],
-                start_temperature = row[12],
-                before_weight = row[13],
-                ideal_weight = row[14],
-                expect_dehydration = row[15],
-                transfusion = row[16],
-                food = row[17],
-                estimate_dehydration = row[18],
-                set_dehydration = row[19],
-                after_weight = row[20],
-                real_dehydration = row[21],
-                start_SBP = row[22],
-                start_DBP = row[23],
-                end_SBP = row[24],
-                end_DBP = row[25],
-                mode = row[26],
-                machine = row[27],
-                start_flow_speed = row[28],
-                start_blood_speed = row[29],
-                Ca = row[30],
-                conductivity = row[31],
-                channel = row[32],
-                heparin = row[33],
-                ESA = row[34],
-                coagulation = row[35],
-                SBP = row[36],
-                DBP = row[37],
-                pulse = row[38],
-                breath = row[39],
-                blood_speed = row[40],
-                flow_speed = row[41],
-                CVP = row[42],
-                DP = row[43],
-                TMP = row[44],
-                dehydrate_speed = row[45],
-                accumulation = row[46],
-                dialyse_temperature = row[47],
-                heparin_volume = row[48],
-                flush = row[49],
-                channel_confirmed = row[50],
+                d_id = d_id,
+                record_time = row[3],
+                SBP = Decimal(row[4]),
+                DBP = row[5],
+                pulse = row[6],
+                breath = Decimal(row[7]),
+                blood_speed = Decimal(row[8]),
+                flow_speed = row[9],
+                CVP = row[10],
+                DP = Decimal(row[11]),
+                TMP = Decimal(row[12]),
+                dehydrate_speed = Decimal(row[13]),
+                accumulation = Decimal(row[14]),
+                dialyse_temperature = Decimal(row[15]),
+                heparin_volume = Decimal(row[16]),
+                flush = row[17],
+                channel_confirmed = row[18],
             )
-        records.append(record)
-        if len(records) > 5000:
-            Record.objects.bulk_create(records)    
-            records = []
+            records.append(record)
+            if len(records) > 5000:
+                Record.objects.bulk_create(records)         # 減少儲存次數
+                records = []
         if records:
             Record.objects.bulk_create(records)
-    end_time = timezone.now()
-    print("Loading CSV took: " + (end_time-start_time).total_seconds() + " seconds.")
