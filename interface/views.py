f20168be67d045ae3f6aa8e21ae598c79722a277
niomoo <jennyliu.lyh@gmail.com>
2022-11-10 22:14:46 +0800

    [Feature]Get data from db and render to template successfully

--- interface/views.py
+++ interface/views.py
@@ -7,19 +7,117 @@ from interface.models import Patient, Dialysis, Record
 # Create your views here.
 
 time = datetime(2022, 7, 5, 8, 30, 12)
-bed = ['A1', 'A2', 'A3', 'A5', 'A6', 'A7', 'A8', 'A9', 
-       'B1', 'B2', 'B3', 'B5', 'B6', 'B7', 'B8', 'B9', 
-       'C1', 'C2', 'C3', 'C5', 'C6', 'C7', 'C8', 'C9', 
-       'D1', 'D2', 'D3', 'D5', 'D6', 'D7', 'D8', 'D9', 
-       'E1', 'E2', 'E3', 'E5', 'E6', 'E7', 'E8', 
-       'I1', 'I2']
+
+b_area = ['B5', 'B9', 'B3', 'B8', 'B2', 'B7', 'B1', 'B6']
+c_area = ['C5', 'C9', 'C3', 'C8', 'C2', 'C7', 'C1', 'C6']
+d_area = ['D5', 'D9', 'D3', 'D8', 'D2', 'D7', 'D1', 'D6']
+a_area = ['A9', '', 'A5', '', 'A3', 'A8', 'A2', 'A7', 'A1', 'A6']
+e_area = ['', '', 'E5', 'E8', 'E3', 'E7', 'E2', 'E6', 'E1', '']
+i_area = ['', '', 'I2', '', 'I1', '']
 
 def index(request):
-    patients = get_patient()
+    patients = get_patients()
     return render(request, 'index.html', {
-        "patients": patients,
+        "a_patients": patients["a_patients"],
+        "b_patients": patients["b_patients"],
+        "c_patients": patients["c_patients"],
+        "d_patients": patients["d_patients"],
+        "e_patients": patients["e_patients"],
+        "i_patients": patients["i_patients"],
     })
 
-def get_patient():
-    patients = Dialysis.objects.filter(start_time__lte=time, end_time__gte=time)
-    return len(patients)
+def get_patients():
+    now_dialysis = Dialysis.objects.filter(start_time__lte=time, end_time__gte=time)
+    patients = {}
+    a_patients = []
+    b_patients = []
+    c_patients = []
+    d_patients = []
+    e_patients = []
+    i_patients = []
+    for bed in a_area:
+        patient = {}
+        patient = {'bed': bed}
+        for d in now_dialysis:
+            if bed == d.bed:
+                start_time = d.start_time
+                patient['id'] = Patient.objects.filter(p_id=d.p_id.p_id)[0]
+                patient['setting'] = d
+                patient['record'] = Record.objects.filter(d_id=d.d_id, record_time__gte=start_time, record_time__lte=time)[0]
+                continue
+        if 'id' not in patient:
+            patient['id'] = '---'
+        a_patients.append(patient)
+    for bed in b_area:
+        patient = {}
+        patient = {'bed': bed}
+        for d in now_dialysis:
+            if bed == d.bed:
+                start_time = d.start_time
+                patient['id'] = Patient.objects.filter(p_id=d.p_id.p_id)[0]
+                patient['setting'] = d
+                patient['record'] = Record.objects.filter(d_id=d.d_id, record_time__gte=start_time, record_time__lte=time)[0]
+                continue
+        if 'id' not in patient:
+            patient['id'] = '---'
+        b_patients.append(patient)
+    for bed in c_area:
+        patient = {}
+        patient = {'bed': bed}
+        for d in now_dialysis:
+            if bed == d.bed:
+                start_time = d.start_time
+                patient['id'] = Patient.objects.filter(p_id=d.p_id.p_id)[0]
+                patient['setting'] = d
+                patient['record'] = Record.objects.filter(d_id=d.d_id, record_time__gte=start_time, record_time__lte=time)[0]
+                continue
+        if 'id' not in patient:
+            patient['id'] = '---'
+        c_patients.append(patient)
+    for bed in d_area:
+        patient = {}
+        patient = {'bed': bed}
+        for d in now_dialysis:
+            if bed == d.bed:
+                start_time = d.start_time
+                patient['id'] = Patient.objects.filter(p_id=d.p_id.p_id)[0]
+                patient['setting'] = d
+                patient['record'] = Record.objects.filter(d_id=d.d_id, record_time__gte=start_time, record_time__lte=time)[0]
+                continue
+        if 'id' not in patient:
+            patient['id'] = '---'
+        d_patients.append(patient)
+    for bed in e_area:
+        patient = {}
+        patient = {'bed': bed}
+        for d in now_dialysis:
+            if bed == d.bed:
+                start_time = d.start_time
+                patient['id'] = Patient.objects.filter(p_id=d.p_id.p_id)[0]
+                patient['setting'] = d
+                patient['record'] = Record.objects.filter(d_id=d.d_id, record_time__gte=start_time, record_time__lte=time)[0]
+                continue
+        if 'id' not in patient:
+            patient['id'] = '---'
+        e_patients.append(patient)
+    for bed in i_area:
+        patient = {}
+        patient = {'bed': bed}
+        for d in now_dialysis:
+            if bed == d.bed:
+                start_time = d.start_time
+                patient['id'] = Patient.objects.filter(p_id=d.p_id.p_id)[0]
+                patient['setting'] = d
+                patient['record'] = Record.objects.filter(d_id=d.d_id, record_time__gte=start_time, record_time__lte=time)[0]
+                continue
+        if 'id' not in patient:
+            patient['id'] = '---'
+        i_patients.append(patient)
+    return {
+        'a_patients': a_patients, 
+        'b_patients': b_patients, 
+        'c_patients': c_patients,
+        'd_patients': d_patients,
+        'e_patients': e_patients,
+        'i_patients': i_patients,
+    }
