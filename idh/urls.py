28af8cc4237a110e608286f7f511af6fe11a02ba
niomoo <jennyliu.lyh@gmail.com>
2022-11-23 18:31:48 +0800

    [Feature]Add line plot to the modal

--- idh/urls.py
+++ idh/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("index/", include('interface.urls')),
+    path("", RedirectView.as_view(url="/index/", permanent=True)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
