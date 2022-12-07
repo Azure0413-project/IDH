500e6f44b12730b6f26de3add7235285e04b9e8e
niomoo <jennyliu.lyh@gmail.com>
2022-12-07 12:13:54 +0800

    [Feature]Add feedback display layout

--- interface/urls.py
+++ interface/urls.py
@@ -4,4 +4,5 @@ from interface import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('get_detail/<str:bed>/<int:idh>', views.get_detail, name='get_detail'),
+    path('get_record', views.get_record, name='get_record'),
 ]
