8f90abb559fd1e7c813e3ebcbbe99c5728e06d2f
kuma0416 <a24350845@gmail.com>
2024-07-24 16:21:17 +0800

    [Feature]Update the nurse area function ,footer and the random code 0 functions.

--- static/js/nurseAreaSearch.js
+++ static/js/nurseAreaSearch.js
@@ -20,25 +20,64 @@ function SwitchNurseList(){
   location.href = targetUrl;
 }
 
-function ClickOnPatient(bed, idh, name, mode, done, first_click) {
-  if (idh > 85 && done == 'False' && first_click == 'True') {
+function ClickOnPatient(bed, idh, name, mode, done, first_click, SBP, DBP, random_code) {
+  console.log("FIRST_CLICK:", first_click);
+  SBP = parseInt(SBP);
+  DBP = parseInt(DBP);
+  random_code = parseInt(random_code);
+  if (random_code == 1 && idh > 85 && done == 'False' && first_click == 'True') {
     let warningModal = document.getElementById("warningModal");
-    pBed = document.getElementById("patientBed");
-    pName = document.getElementById("patientName");
-    pBed.innerText = bed;
-    pName.innerText = name;
+    document.getElementById("warning-left-SBP").style.display='';
+    document.getElementById("warning-left-DBP").style.display='';
+    document.getElementById("warning-right").style.display='';
+    document.getElementById("confirmWarningBtn").style.display='';
+    document.getElementById("warningClickBtn").style.display='None';
+    document.getElementById("patientBed").innerText = bed;
+    document.getElementById("patientName").innerText = name;
+    document.getElementById("SBP").value = SBP;
+    document.getElementById("DBP").value = DBP;
     console.log("danger");
     warningModal.classList.toggle("hidden");
-  } else if (idh > 85 && done == 'False' && first_click == 'False') {
-    $.get(rootUrl+`warning_click/${bed}/${name}`, ()=>{ //0416
-      console.log("first warning click.");
-    });
-    location.reload();
+  } else if (random_code == 1 && idh > 85 && done == 'False' && first_click == 'False') {
+    warningModal.classList.toggle("hidden");
+    document.getElementById("warning-left-SBP").style.display='None';
+    document.getElementById("warning-left-DBP").style.display='None';
+    document.getElementById("warning-right").style.display='None';
+    document.getElementById("confirmWarningBtn").style.display='None';
+    document.getElementById("warningClickBtn").style.display='';
+    document.getElementById("patientBed").innerText = bed;
+    document.getElementById("patientName").innerText = name;
+    document.getElementById("SBP").value = SBP;
+    document.getElementById("DBP").value = DBP;
+    // $.get(rootUrl+`warning_click/${bed}/${name}`, ()=>{ //0416
+    //   console.log("first warning click.");
+    // });
+    // location.reload();
   } else {
     let nurseId = document.getElementById('nurseId').value;
     location.href = rootUrl + `get_nurse_detail/${nurseId}/${bed}/${idh}`;
   }
 }
+
+// function ClickOnPatient(bed, idh, name, mode, done, first_click) {
+//   if (idh > 70 && done == 'False' && first_click == 'True') {
+//     let warningModal = document.getElementById("warningModal");
+//     pBed = document.getElementById("patientBed");
+//     pName = document.getElementById("patientName");
+//     pBed.innerText = bed;
+//     pName.innerText = name;
+//     console.log("danger");
+//     warningModal.classList.toggle("hidden");
+//   } else if (idh > 70 && done == 'False' && first_click == 'False') {
+//     $.get(rootUrl+`warning_click/${bed}/${name}`, ()=>{ //0416
+//       console.log("first warning click.");
+//     });
+//     location.reload();
+//   } else {
+//     let nurseId = document.getElementById('nurseId').value;
+//     location.href = rootUrl + `get_nurse_detail/${nurseId}/${bed}/${idh}`;
+//   }
+// }
 
 window.onclick = function (event) {
   if (event.target.id == "modal") {
@@ -67,7 +106,7 @@ function liClickEvent(e) {
   
 function CloseWarningModal() {
     document.getElementById("warningModal").classList.toggle("hidden");
-    SearchFunc();
+    // SearchFunc();
 }
 
 function SubmitWarning() {
@@ -104,4 +143,40 @@ function SubmitWarning() {
         console.log(res);
       },
     });
+}
+
+function SubmitWarningClick() {
+  let pBed = document.getElementById("patientBed").innerText;
+  let pName = document.getElementById("patientName").innerText;
+  let formData = new FormData(document.getElementById("warningReport"));
+  let originLocation = location.href;
+  formData.append("patientBed", pBed);
+  formData.append("patientName", pName);
+  result = {};
+  for (let p of formData.entries()) {
+    result[p[0]] = p[1];
   }
+  $.ajax({
+    url: rootUrl+"warning_click/",
+    method: "POST",
+    headers: {
+      "X-CSRFToken": $('[name="csrf-token"]').attr("content"),
+    },
+    dataType: "json",
+    data: result,
+    success: (res) => {
+      if (res["status"] == "success") {
+        // 頁面跳轉
+        alert("Add success.");
+        // 記錄床號與時間
+        location.href = originLocation;
+      } else {
+        // 畫面提醒 送出表單失敗
+        alert("Add fail.\n" + res["msg"]);
+      }
+    },
+    error: (res) => {
+      console.log(res);
+    },
+  });
+}
