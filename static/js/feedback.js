a25a9dd4aa4b34b97f8f37c2e959b3bb036e2f95
kuma0416 <a24350845@gmail.com>
2024-08-06 19:01:03 +0800

    [Feature]Add threshold const variable, warning feedback layout and time adjust.

--- static/js/feedback.js
+++ static/js/feedback.js
@@ -2,13 +2,16 @@ function TimeAppend(p_id){
     console.log(p_id);
     let idh_time_list = document.getElementById(`idh-time-${p_id}`);
     let idh_time = document.getElementById(`idh-select-${p_id}`).value;
-    // idh_time_list.value = idh_time_list.value + "test";
-    // console.log(`time list: ${idh_time_list}, time: ${idh_time}`);
-    if(idh_time_list.value.length != 0){
-        idh_time_list.value += ",";
+    current_time = idh_time.replace(":", "");
+    current_time_set = new Set(idh_time_list.value.split(","));
+    if(current_time_set.has(current_time)){
+        current_time_set.delete(current_time);
+        idh_time_list.value = [...current_time_set].join(",");
+    } else {
+        if(idh_time_list.value.length != 0){
+            idh_time_list.value += ",";
+        } 
+        idh_time_list.value += `${idh_time.replace(":", "")}`;
     }
-    // idh_time_list += "test";
-    idh_time_list.value += `${idh_time.replace(":", "")}`;
-    // console.log(`time list: ${idh_time_list}, time: ${idh_time}`);
     return;
 }
